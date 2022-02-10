4ba26e3def70a00dd3fefe389333e94018f9c861
PLANCQ Valentin <sulfeur84@gmail.com>
2022-02-10 17:55:53 +0100

    Module C plus dur :3

--- Assets/Scripts/ModuleC.cs
+++ Assets/Scripts/ModuleC.cs
@@ -1,12 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ModuleC : MonoBehaviour
 {
     
     public Renderer Voyant;
     public Material Vert;
+
+    private int a, b;
+    public TextMeshProUGUI aT, bT;
+    private int True, False1, False2;
+    public TextMeshProUGUI T1, F1, F2;
+
+    public bool D, M, G;
+
+    public void Awake()
+    {
+        a = Random.Range(1, 100);
+        b = Random.Range(1, 100);
+
+        aT.text = a.ToString();
+        bT.text = b.ToString();
+
+        if (G)
+        {
+            True = a + b;
+            False1 = a + b + 1;
+            False2 = a + b + 2;
+        }
+        if (M)
+        {
+            True = a + b;
+            False1 = a + b - 1;
+            False2 = a + b + 1;
+        }
+        if (D)
+        {
+            True = a + b;
+            False1 = a + b - 1;
+            False2 = a + b - 2;
+        }
+
+        T1.text = True.ToString();
+        F1.text = False1.ToString();
+        F2.text = False2.ToString();
+
+    }
 
     public void InstantWin()
     {
